17de19243cc5990f910a5ab08371b3aa4867a2b6
AlexPrediger <39220443+AlexPrediger@users.noreply.github.com>
2019-09-17 10:01:24 +0200

    splited demo and component

--- dist/ui-notification.js
+++ dist/ui-notification.js
@@ -91,34 +91,10 @@ return /******/ (function(modules) { // webpackBootstrap
 /******/
 /******/
 /******/ 	// Load entry module and return exports
-/******/ 	return __webpack_require__(__webpack_require__.s = "./js/src/index.js");
+/******/ 	return __webpack_require__(__webpack_require__.s = "./src/js/index.js");
 /******/ })
 /************************************************************************/
 /******/ ({
-
-/***/ "./js/src/index.js":
-/*!*************************!*\
-  !*** ./js/src/index.js ***!
-  \*************************/
-/*! exports provided: init */
-/***/ (function(module, __webpack_exports__, __webpack_require__) {
-
-"use strict";
-eval("__webpack_require__.r(__webpack_exports__);\n/* harmony export (binding) */ __webpack_require__.d(__webpack_exports__, \"init\", function() { return init; });\n/* harmony import */ var _notification__WEBPACK_IMPORTED_MODULE_0__ = __webpack_require__(/*! ./notification */ \"./js/src/notification.js\");\n\n/**\n * init a notification\n * @param {String} title\n * @param {String} subtitle\n * @param {Object} config\n * @return {Class}\n */\n\nvar init = function init(title, subtitle) {\n  var config = arguments.length > 2 && arguments[2] !== undefined ? arguments[2] : {};\n  return new _notification__WEBPACK_IMPORTED_MODULE_0__[\"default\"](title, subtitle, config);\n};\n\n\n\n//# sourceURL=webpack://UiNotifcation/./js/src/index.js?");
-
-/***/ }),
-
-/***/ "./js/src/notification.js":
-/*!********************************!*\
-  !*** ./js/src/notification.js ***!
-  \********************************/
-/*! exports provided: default */
-/***/ (function(module, __webpack_exports__, __webpack_require__) {
-
-"use strict";
-eval("__webpack_require__.r(__webpack_exports__);\n/* harmony import */ var create_element_lib__WEBPACK_IMPORTED_MODULE_0__ = __webpack_require__(/*! create-element-lib */ \"./node_modules/create-element-lib/src/index.js\");\nfunction _classCallCheck(instance, Constructor) {\n  if (!(instance instanceof Constructor)) {\n    throw new TypeError(\"Cannot call a class as a function\");\n  }\n}\n\nfunction _defineProperties(target, props) {\n  for (var i = 0; i < props.length; i++) {\n    var descriptor = props[i];\n    descriptor.enumerable = descriptor.enumerable || false;\n    descriptor.configurable = true;\n    if (\"value\" in descriptor) descriptor.writable = true;\n    Object.defineProperty(target, descriptor.key, descriptor);\n  }\n}\n\nfunction _createClass(Constructor, protoProps, staticProps) {\n  if (protoProps) _defineProperties(Constructor.prototype, protoProps);\n  if (staticProps) _defineProperties(Constructor, staticProps);\n  return Constructor;\n}\n\n\n\nvar Notification =\n/*#__PURE__*/\nfunction () {\n  function Notification(title, subtitle) {\n    var config = arguments.length > 2 && arguments[2] !== undefined ? arguments[2] : {};\n\n    _classCallCheck(this, Notification);\n\n    if (!title || !subtitle) {\n      throw new Error('Cannot create component Notification');\n    }\n\n    this.title = title;\n    this.subtitle = subtitle;\n    this.config = config;\n    this.options = this.applyOptions();\n    this.template = null;\n    this.createTemplate();\n    this.render();\n  }\n\n  _createClass(Notification, [{\n    key: \"applyOptions\",\n    value: function applyOptions() {\n      var defaultOption = {\n        root: document.querySelector('body'),\n        shadow: false,\n        closable: true,\n        duration: null,\n        shouldDestroy: true\n      };\n      return Object.assign({}, defaultOption, this.config);\n    }\n  }, {\n    key: \"createTemplate\",\n    value: function createTemplate() {\n      var _this = this;\n\n      var shadow;\n      var button;\n\n      if (this.options.shadow) {\n        shadow = Object(create_element_lib__WEBPACK_IMPORTED_MODULE_0__[\"h\"])('div', {\n          \"class\": 'shadow'\n        });\n      }\n\n      if (this.options.closable) {\n        button = Object(create_element_lib__WEBPACK_IMPORTED_MODULE_0__[\"h\"])('button', {\n          \"class\": 'button',\n          click: function click() {\n            _this.destroy();\n          }\n        }, ['Schließen']);\n      }\n\n      this.template = Object(create_element_lib__WEBPACK_IMPORTED_MODULE_0__[\"h\"])('div', {\n        \"class\": 'js--notification'\n      }, [shadow, Object(create_element_lib__WEBPACK_IMPORTED_MODULE_0__[\"h\"])('div', {\n        \"class\": 'box'\n      }, [Object(create_element_lib__WEBPACK_IMPORTED_MODULE_0__[\"h\"])('p', {\n        \"class\": 'title'\n      }, [this.title]), Object(create_element_lib__WEBPACK_IMPORTED_MODULE_0__[\"h\"])('p', {\n        \"class\": 'subtitle'\n      }, [this.subtitle]), button])]);\n    }\n  }, {\n    key: \"render\",\n    value: function render() {\n      var _this2 = this;\n\n      if (!this.template || !this.options.root) {\n        return;\n      }\n\n      if (this.options.beforeRender) {\n        this.options.beforeRender(this, this.template);\n      }\n\n      this.options.root.appendChild(this.template);\n\n      if (this.options.afterRender) {\n        this.options.afterRender(this, this.template);\n      }\n\n      if (this.options.duration > 0) {\n        setTimeout(function () {\n          _this2.destroy();\n        }, this.options.duration);\n      }\n    }\n  }, {\n    key: \"destroy\",\n    value: function destroy() {\n      if (!this.template) {\n        return;\n      }\n\n      if (this.options.beforeDestroy) {\n        this.options.beforeDestroy(this, this.template);\n      }\n\n      if (this.options.shouldDestroy) {\n        this.template.parentElement.removeChild(this.template);\n      }\n\n      if (this.options.afterDestroy) {\n        this.options.afterDestroy(this);\n      }\n    }\n  }]);\n\n  return Notification;\n}();\n\n/* harmony default export */ __webpack_exports__[\"default\"] = (Notification);\n\n//# sourceURL=webpack://UiNotifcation/./js/src/notification.js?");
-
-/***/ }),
 
 /***/ "./node_modules/create-element-lib/src/h.js":
 /*!**************************************************!*\
@@ -153,6 +129,30 @@ eval("__webpack_require__.r(__webpack_exports__);\n/* harmony export (binding) *
 
 "use strict";
 eval("__webpack_require__.r(__webpack_exports__);\n/* harmony export (binding) */ __webpack_require__.d(__webpack_exports__, \"isString\", function() { return isString; });\n/**\n * checks if a given arg is a string\n * @type {String} s\n * @return {Boolean}\n */\nconst isString = s => typeof s === 'string';\n\n\n//# sourceURL=webpack://UiNotifcation/./node_modules/create-element-lib/src/is-string.js?");
+
+/***/ }),
+
+/***/ "./src/js/index.js":
+/*!*************************!*\
+  !*** ./src/js/index.js ***!
+  \*************************/
+/*! exports provided: init */
+/***/ (function(module, __webpack_exports__, __webpack_require__) {
+
+"use strict";
+eval("__webpack_require__.r(__webpack_exports__);\n/* harmony export (binding) */ __webpack_require__.d(__webpack_exports__, \"init\", function() { return init; });\n/* harmony import */ var _notification__WEBPACK_IMPORTED_MODULE_0__ = __webpack_require__(/*! ./notification */ \"./src/js/notification.js\");\n\n/**\r\n * init a notification\r\n * @param {String} title\r\n * @param {String} subtitle\r\n * @param {Object} config\r\n * @return {Class}\r\n */\n\nvar init = function init(title, subtitle) {\n  var config = arguments.length > 2 && arguments[2] !== undefined ? arguments[2] : {};\n  return new _notification__WEBPACK_IMPORTED_MODULE_0__[\"default\"](title, subtitle, config);\n};\n\n\n\n//# sourceURL=webpack://UiNotifcation/./src/js/index.js?");
+
+/***/ }),
+
+/***/ "./src/js/notification.js":
+/*!********************************!*\
+  !*** ./src/js/notification.js ***!
+  \********************************/
+/*! exports provided: default */
+/***/ (function(module, __webpack_exports__, __webpack_require__) {
+
+"use strict";
+eval("__webpack_require__.r(__webpack_exports__);\n/* harmony import */ var create_element_lib__WEBPACK_IMPORTED_MODULE_0__ = __webpack_require__(/*! create-element-lib */ \"./node_modules/create-element-lib/src/index.js\");\nfunction _classCallCheck(instance, Constructor) {\n  if (!(instance instanceof Constructor)) {\n    throw new TypeError(\"Cannot call a class as a function\");\n  }\n}\n\nfunction _defineProperties(target, props) {\n  for (var i = 0; i < props.length; i++) {\n    var descriptor = props[i];\n    descriptor.enumerable = descriptor.enumerable || false;\n    descriptor.configurable = true;\n    if (\"value\" in descriptor) descriptor.writable = true;\n    Object.defineProperty(target, descriptor.key, descriptor);\n  }\n}\n\nfunction _createClass(Constructor, protoProps, staticProps) {\n  if (protoProps) _defineProperties(Constructor.prototype, protoProps);\n  if (staticProps) _defineProperties(Constructor, staticProps);\n  return Constructor;\n}\n\n\n\nvar Notification =\n/*#__PURE__*/\nfunction () {\n  function Notification(title, subtitle) {\n    var config = arguments.length > 2 && arguments[2] !== undefined ? arguments[2] : {};\n\n    _classCallCheck(this, Notification);\n\n    if (!title || !subtitle) {\n      throw new Error('Cannot create component Notification');\n    }\n\n    this.title = title;\n    this.subtitle = subtitle;\n    this.config = config;\n    this.options = this.applyOptions();\n    this.template = null;\n    this.createTemplate();\n    this.render();\n  }\n\n  _createClass(Notification, [{\n    key: \"applyOptions\",\n    value: function applyOptions() {\n      var defaultOption = {\n        root: document.querySelector('body'),\n        shadow: false,\n        closable: true,\n        duration: null,\n        shouldDestroy: true\n      };\n      return Object.assign({}, defaultOption, this.config);\n    }\n  }, {\n    key: \"createTemplate\",\n    value: function createTemplate() {\n      var _this = this;\n\n      var shadow;\n      var button;\n\n      if (this.options.shadow) {\n        shadow = Object(create_element_lib__WEBPACK_IMPORTED_MODULE_0__[\"h\"])('div', {\n          \"class\": 'shadow'\n        });\n      }\n\n      if (this.options.closable) {\n        button = Object(create_element_lib__WEBPACK_IMPORTED_MODULE_0__[\"h\"])('button', {\n          \"class\": 'button',\n          click: function click() {\n            _this.destroy();\n          }\n        }, ['Schließen']);\n      }\n\n      this.template = Object(create_element_lib__WEBPACK_IMPORTED_MODULE_0__[\"h\"])('div', {\n        \"class\": 'js--notification'\n      }, [shadow, Object(create_element_lib__WEBPACK_IMPORTED_MODULE_0__[\"h\"])('div', {\n        \"class\": 'box'\n      }, [Object(create_element_lib__WEBPACK_IMPORTED_MODULE_0__[\"h\"])('p', {\n        \"class\": 'title'\n      }, [this.title]), Object(create_element_lib__WEBPACK_IMPORTED_MODULE_0__[\"h\"])('p', {\n        \"class\": 'subtitle'\n      }, [this.subtitle]), button])]);\n    }\n  }, {\n    key: \"render\",\n    value: function render() {\n      var _this2 = this;\n\n      if (!this.template || !this.options.root) {\n        return;\n      }\n\n      if (this.options.beforeRender) {\n        this.options.beforeRender(this, this.template);\n      }\n\n      this.options.root.appendChild(this.template);\n\n      if (this.options.afterRender) {\n        this.options.afterRender(this, this.template);\n      }\n\n      if (this.options.duration > 0) {\n        setTimeout(function () {\n          _this2.destroy();\n        }, this.options.duration);\n      }\n    }\n  }, {\n    key: \"destroy\",\n    value: function destroy() {\n      if (!this.template) {\n        return;\n      }\n\n      if (this.options.beforeDestroy) {\n        this.options.beforeDestroy(this, this.template);\n      }\n\n      if (this.options.shouldDestroy) {\n        this.template.parentElement.removeChild(this.template);\n      }\n\n      if (this.options.afterDestroy) {\n        this.options.afterDestroy(this);\n      }\n    }\n  }]);\n\n  return Notification;\n}();\n\n/* harmony default export */ __webpack_exports__[\"default\"] = (Notification);\n\n//# sourceURL=webpack://UiNotifcation/./src/js/notification.js?");
 
 /***/ })
 
